8aa5c64da4b3e99ec2391eb53b3976f83a2e5af2
chenxinfan <1273767427@qq.com>
2023-12-19 22:10:54 +0800

    init

---
+++ java-build-html-common/src/main/java/cn/chenxinfan/comon/Main.java
@@ -0,0 +1,12 @@
+package cn.chenxinfan.comon;
+
+/**
+ * @author chenxinfan
+ * @site www.chenxinfan.cn
+ * @date 2023/12/19 22:07
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
